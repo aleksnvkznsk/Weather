97d5b499c9c0574970b271fec649a64137bda6e5
aleksnvkznsk <aleksandrov.developer@gmail.com>
2023-06-25 21:01:13 +0700

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-function App() {
-  return (
-    <></>
-  )
+import React from "react";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Form />
+        <Weather />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
